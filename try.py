3b2537ab25a7393bf70fa75c1fd8d9bdbbfa959e
zhangpan721 <zhangpan721@hotmail.com>
2018-01-04 09:58:52 -0500

    aaaa
    adddddd

---
+++ try.py
@@ -0,0 +1,10 @@
+import csv 
+
+with open('data.csv') as csvfile:
+	readCSV = csv.reader (csvfile,delimiter=',')
+#	for row in readCSV:
+#		print row
+#		print row[0]
+#		print row[0],row[1]
+print type(readCSV)
+print readCSV.shape
